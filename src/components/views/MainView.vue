1c551c6fbce26704a3d200c583187cd3220d161b
YW-712 <u38141@hs-harz.de>
2026-05-28 01:40:41 +0200

    drag window

--- src/components/views/MainView.vue
+++ src/components/views/MainView.vue
@@ -5,12 +5,12 @@ import TaskView from "./TaskView.vue";
 import CalenderView from "./CalenderView.vue";
 import Navbar from "../bars/Navbar.vue";
 import Footer from "../bars/Footer.vue";
-import { watch, nextTick } from "vue";
+import {watch, nextTick} from "vue";
 
 const siteState = ref(0);
 provide("siteState", siteState);
 onMounted(() => {
-  const main = document.getElementById("nav-window");
+  const main = document.getElementById("main-container");
   const test = document.getElementById("dashboard-window");
 
   if (main) dragElement(main);
@@ -19,12 +19,22 @@ onMounted(() => {
 
 function dragElement(elmnt) {
   let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  elmnt.onmousedown = dragMouseDown;
+
+  // Nur die Title-Bar draggable machen
+  const header = elmnt.querySelector(".aero-title-bar");
+
+  if (header) {
+    header.onmousedown = dragMouseDown;
+  } else {
+    elmnt.onmousedown = dragMouseDown;
+  }
 
   function dragMouseDown(e) {
     e.preventDefault();
+
     pos3 = e.clientX;
     pos4 = e.clientY;
+
     document.onmouseup = closeDragElement;
     document.onmousemove = elementDrag;
   }
@@ -44,7 +54,6 @@ function dragElement(elmnt) {
     let newTop = elmnt.offsetTop - pos2;
     let newLeft = elmnt.offsetLeft - pos1;
 
-    // untere Grenze = Footer
     const maxTop = footerTop - elmnt.offsetHeight;
 
     newTop = Math.max(0, Math.min(maxTop, newTop));
@@ -58,6 +67,8 @@ function dragElement(elmnt) {
     document.onmousemove = null;
   }
 }
+
+
 const showTestContainer = ref(true);
 provide("showTestContainer", showTestContainer);
 watch(siteState, (newVal) => {
@@ -79,49 +90,50 @@ watch(showTestContainer, (val) => {
   <div id="main-wrapper">
     <Navbar/>
 
-    <div id="main-container" >
-      <div class="aero-window" id="nav-window" >
-        <div class="aero-title-bar">
-          <span class="aero-title-icon"></span>
-          <span class="aero-title-bar-text">Navbar</span>
-          <div class="aero-title-bar-controls">
-          </div>
-        </div>
-        <div class="aero-window-body">
-
+    <div class="glas main-container" id="main-container">
           <div id="navbar-container">
+            <div class="aero-title-bar">
+
+              <span class="aero-title-bar-text">Dashboard</span>
             <div class="navbar-item">
-              <button class="frutiger-aero-button small" type="button" v-on:click="siteState = 0; console.log(siteState)">Dashboard</button>
+              <button class="frutiger-aero-button small" type="button"
+                      v-on:click="siteState = 0; console.log(siteState)">Dashboard
+              </button>
             </div>
             <div class="navbar-item">
-              <button class="frutiger-aero-button small" type="button" v-on:click="siteState = 1; console.log(siteState)">Aufgaben</button>
+              <button class="frutiger-aero-button small" type="button"
+                      v-on:click="siteState = 1; console.log(siteState)">Aufgaben
+              </button>
             </div>
             <div class="navbar-item">
-              <button class="frutiger-aero-button small" v-on:click="siteState = 2; console.log(siteState)">Kalender</button>
+              <button class="frutiger-aero-button small" v-on:click="siteState = 2; console.log(siteState)">Kalender
+              </button>
             </div>
           </div>
-
-        </div>
-      </div>
+          Test test test
+    </div>
     </div>
 
-    <div id="dashboard-container" >
-      <div class="aero-window" id="dashboard-window" v-if="showTestContainer">
-        <div class="aero-title-bar">
-          <span class="aero-title-icon"></span>
-          <span class="aero-title-bar-text">Dashboard</span>
-          <div class="aero-title-bar-controls">
-            <button aria-label="Minimize">&#8212;</button>
-            <button aria-label="Maximize">&#9633;</button>
-            <button aria-label="Close" class="close" @click="showTestContainer = false">&#215;</button>
+
+
+
+
+    <div class="glas dashboard-container" id="dashboard-container">
+        <div class="aero-window" id="dashboard-window" v-if="showTestContainer">
+          <div class="aero-title-bar">
+
+            <span class="aero-title-bar-text">Dashboard</span>
+
+              <button aria-label="Close" class="close" @click="showTestContainer = false">&#215;</button>
+            </div>
+
+          <div class="aero-window-body">
+
+            <DashboardView v-if="siteState === 0"/>
+
           </div>
         </div>
-        <div class="aero-window-body">
 
-          <DashboardView v-if="siteState === 0"/>
-
-        </div>
-      </div>
     </div>
     <Footer id="app-footer"></Footer>
   </div>
